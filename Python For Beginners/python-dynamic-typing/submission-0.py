094661a6da6fc06c8be6b49bb2bad53f6bec8634
Sa-leem Kamarah <78922277+SKamarah@users.noreply.github.com>
2026-05-09 23:02:03 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,14 @@
+variable = 10
+print(type(variable))
+
+variable = 10.00
+print(type(variable))
+
+variable = False
+print(type(variable))
+
+variable = "Test"
+print(type(variable))
+
+variable = [1,2]
+print(type(variable))
