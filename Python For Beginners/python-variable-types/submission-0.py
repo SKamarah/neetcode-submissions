79a8f6758ae34c4f778ac072d6b0a9ab0c009506
Sa-leem Kamarah <78922277+SKamarah@users.noreply.github.com>
2026-05-09 22:42:19 -0400

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+interger_type = 10
+float_type = 10.00
+bool_type = True
+string_type = "Test"
+list_type = [1,2,3]
+
+
+
+
+print(type(10))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
